a560c8619ef05b225230d2af0ab2ccf7a3880cc1
hky3535 <hky3535@163.com>
2023-07-25 05:20:05 +0000

    update docker run

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,5 @@ FROM ubuntu:latest
 
 COPY . /workspace/online_toolkit
 
-RUN apt-get update && \
-    apt-get install -y python3.10 python3-pip libglib2.0-dev libgl1-mesa-glx
-
 WORKDIR /workspace/online_toolkit
 CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+# apt安装所需包
+apt update && apt install -y $(cat requirements.apt)
+# pip安装所需包
 python3 -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+# 启动程序
 python3 manage.py runsslserver --cert ssl/cert.pem --key ssl/key.pem 0.0.0.0:30000
